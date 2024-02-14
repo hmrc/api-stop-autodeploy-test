c5cecb7decd121902b23cf7c052288f284cf339c
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2024-02-14 13:05:07 +0000

    APIS-6812: play 3.0 upgrade (#26)
    * APIS-6812: play 3.0 upgrade
    
    * APIS-6812 - Tweak to ensure things appear in Catalogue correctly
    
    ---------
    
    Co-authored-by: Andy Spaven <16537059+AndySpaven@users.noreply.github.com>

--- test/uk/gov/hmrc/apistopautodeploytest/controllers/HelloWorldSpec.scala
+++ test/uk/gov/hmrc/apistopautodeploytest/controllers/HelloWorldSpec.scala
@@ -20,7 +20,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.Future.{failed, successful}
 
-import akka.stream.Materializer
+import org.apache.pekko.stream.Materializer
 import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
