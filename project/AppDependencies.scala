d5b690e5718534582b79064b57f4680b2aee0f34
John Green <john.green@digital.hmrc.gov.uk>
2024-07-22 14:36:17 +0100

    APIS-7211: Upgrade to Java 21 (#29)
    * APIS-7211: Upgrade to Java 21
    
    * APIS-7211: Remove unused scalaj dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  lazy val bootstrapVersion = "8.4.0"
+  lazy val bootstrapVersion = "9.0.0"
 
   lazy val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
@@ -13,7 +13,6 @@ object AppDependencies {
   lazy val test = Seq(
     "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.pegdown"            % "pegdown"                 % "1.6.0",
-    "org.mockito"           %% "mockito-scala-scalatest" % "1.17.29",
-    "org.scalaj"            %% "scalaj-http"             % "2.4.2"
+    "org.mockito"           %% "mockito-scala-scalatest" % "1.17.29"
   ).map(_ % "test")
 }
