30c7a17c03a20907028b00d4fd86fdaad6a09906
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-05-05 09:53:52 +0100

    API-9130 - Bump versions for bobby vulnerabilites (#45)
    * API-9128 - Bump versions for bobby vulnerabilites
    
    * API-9130 - PR Bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  lazy val bootstrapVersion = "9.0.0"
+  lazy val bootstrapVersion = "10.7.0"
 
   lazy val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
@@ -12,7 +12,6 @@ object AppDependencies {
 
   lazy val test = Seq(
     "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "org.pegdown"            % "pegdown"                 % "1.6.0",
     "org.mockito"           %% "mockito-scala-scalatest" % "1.17.29"
   ).map(_ % "test")
 }
