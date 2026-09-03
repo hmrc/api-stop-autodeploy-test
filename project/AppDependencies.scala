b9c0df43e74f676af576fa9af98615a5972d89e1
John Green <john.green@digital.hmrc.gov.uk>
2026-09-03 14:18:19 +0100

    API-9478 - Changing access.type to access (#46)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  lazy val bootstrapVersion = "10.7.0"
+  lazy val bootstrapVersion = "10.8.0"
 
   lazy val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
